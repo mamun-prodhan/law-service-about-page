6856341b6ba6c637d8ee4736f467bd6151782533
mamun-prodhan <mamunpr9@gmail.com>
2023-09-18 19:48:30 +0600

    completed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import banner from "../../src/assets/about-banner.png";
 
 const Banner = () => {
   return (
-    <div className="xl:px-16 xl:pt-8">
+    <div className=" md:px-6 lg:px-14 mid-xl:px-16 md:pt-6 mid-xl:pt-8">
       <img className="w-full" src={banner} alt="banner" />
     </div>
   );
